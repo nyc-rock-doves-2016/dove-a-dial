011bb81037e63ccf0209907b731c36c560312b76
Zack Mance <zmance@gmail.com>
2016-05-03 22:07:37 -0400

    Adds Single Table Inheritence Student Teacher

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501231200) do
+ActiveRecord::Schema.define(version: 20160504004054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,12 +41,18 @@ ActiveRecord::Schema.define(version: 20160501231200) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string   "full_name",       null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "full_name",                           null: false
+    t.string   "email",                               null: false
+    t.string   "password_digest",                     null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "type",            default: "student"
   end
 
 end
